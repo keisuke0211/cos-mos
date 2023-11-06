79a1cb22c966a49e8bd540d7a9a3cc6f6508a870
大殿慶輔 <221111@st.yoshida-g.ac.jp>
2023-11-06 18:45:05 +0900

    ゴールゲートの配置

--- code/Project/System/StageEditor.cpp
+++ code/Project/System/StageEditor.cpp
@@ -464,7 +464,7 @@ void CStageEditor::ObjPlace(float fSizeX, float fSizeY, D3DXVECTOR3 pos, int nTy
 		Manager::BlockMgr()->RoadTripLaserCreate(pos, D3DXVECTOR3(0.0f, 0.0f, 0.0f),0.0f);
 		break;
 	case TYPE_Extenddog:
-		Manager::BlockMgr()->ExtenddogCreate(pos, -200.0f, -200.0f, true);
+		Manager::BlockMgr()->ExtenddogCreate(pos, pos.x, pos.x, true);
 		break;
 	case TYPE_FILL_BLOCK_11:
 		Manager::BlockMgr()->FillBlockCreate(pos, CFillBlock::FILL_TYPE::FILL_1x1, m_StageColor.FillBlock);
@@ -509,7 +509,7 @@ void CStageEditor::ObjPlace(float fSizeX, float fSizeY, D3DXVECTOR3 pos, int nTy
 		CMode_Game::GetPlayer()->SetPos(1, pos);
 		break;
 	case TYPE_GOALGATE:
-		Manager::BlockMgr()->BlockCreate(pos, Color{ 0, 0, 0, 255 });
+		Manager::BlockMgr()->GoalGateCreate(pos);
 		break;
 	case TYPE_PARTS:
 		Manager::BlockMgr()->PartsCreate(pos);
