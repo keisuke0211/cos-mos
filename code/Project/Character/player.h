0187fa9833ec8fba446f6e68238650a1df46fa7f
ShionHirasawa <221053@st.yoshida-g.ac.jp>
2023-11-22 10:27:55 +0900

    プレイヤーにあったコメントアウトしていた当たり判定関数の名残を削除

--- code/Project/Character/player.h
+++ code/Project/Character/player.h
@@ -235,19 +235,6 @@ private:
 	void FixPos_LEFT(float *pPosX, float fMinPosX, float *pMoveX, float fWidth);	// 左からの当たり判定による位置・移動量修正
 	void FixPos_RIGHT(float *pPosX, float fMaxPosX, float *pMoveX, float fWidth);	// 右からの当たり判定による位置・移動量修正
 
-	//void CollisionBlock(Info *pInfo, CollInfo *pColli);
-	//void CollisionFillBlock(Info *pInfo,COLLI_ROT ColliRot);
-	//void CollisionTrampoline(Info *pInfo, CollInfo *pColli, CTrampoline *pTrampoline);
-	//void CollisionSpike(Info *pInfo, CollInfo *pColli);
-	//void CollisionMoveBlock(Info *pInfo, CMoveBlock *pMoveBlock, CollInfo *pColli);
-	//void CollisionMeteor(Info *pInfo, CollInfo *pColli);
-	//void CollisionLaser(Info *pInfo, CRoadTripLaser *pRoadTripLaser, CollInfo *pColli, CollInfo *pOthColli);
-	//void CollisionDog(Info *pInfo, CExtenddog *pExtenddog, CollInfo *pColli, CollInfo *pOthColli);
-	//void CollisionGoalGate(Info *pInfo, CollInfo *pColli);
-	//void CollisionParts(Info *pInfo, CParts *pParts);
-	//void CollisionRocket(Info *pInfo, CRocket *pRocket);
-	//void CollisionPile(Info *pInfo, CollInfo *pColli, CPile *pPile);
-
 	bool IsKeyConfigTrigger(const int nIdx, const WORLD_SIDE side, KEY_CONFIG KeyConfig);
 	bool IsKeyConfigPress(const int nIdx, const WORLD_SIDE side, KEY_CONFIG KeyConfig);
 
