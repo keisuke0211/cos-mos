37a0223800cd31ac2908b07bf4bd95e14469d8c3
AiraOomiya <221007@st.yoshida-g.ac.jp>
2023-11-27 10:47:25 +0900

    環境音調整完了

--- enc_temp_folder/9b5babcad22db721cca4092914a6c80/ambient-sound-player.cpp
+++ enc_temp_folder/9b5babcad22db721cca4092914a6c80/ambient-sound-player.cpp
@@ -21,7 +21,7 @@ struct AmbientSEInfo {
 //****************************************
 // íËêîíËã`
 //****************************************
-#define AMBIENT_SE_MAX (2)
+#define AMBIENT_SE_MAX (3)
 static const AmbientSEInfo sc_ambientSEInfos[AMBIENT_SE_MAX] = {
 	{ "data\\SOUND\\SE\\AmbientSound\\bard.wav", 200, 500, 0.0f, 0.0f },
 	{ "data\\SOUND\\SE\\AmbientSound\\shake.wav", 100, 300, 0.0f, 0.0f },
