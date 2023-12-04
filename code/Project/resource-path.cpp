69e9510ca8586b5e8703a888f1da0837728e9720
SyuukiIida <221070@st.yoshida-g.ac.jp>
2023-12-04 12:10:25 +0900

    SE追加

--- code/Project/resource-path.cpp
+++ code/Project/resource-path.cpp
@@ -11,5 +11,17 @@ const char* CResources::TEXTURE_PATHS[(int)TEXTURE::MAX] = {
 short CResources::SOUND_IDXES[(int)SOUND::MAX] = {};
 const char* CResources::SOUND_PATHS[(int)SOUND::MAX] = {
 	"data\\SOUND\\SE\\ok.wav",
-	"data\\SOUND\\SE\\scratch.wav",
+	"data\\SOUND\\SE\\scratch_02.wav",
+	"data\\SOUND\\SE\\jamp_01.wav",
+	"data\\SOUND\\SE\\jamp.wav",
+	"data\\SOUND\\SE\\swap_now.wav",
+	"data\\SOUND\\SE\\swap_end.wav",
+	"data\\SOUND\\SE\\death_expand.wav",
+	"data\\SOUND\\SE\\death_explosion.wav",
+	"data\\SOUND\\SE\\heron2.wav",
+	"data\\SOUND\\SE\\shrink.wav",
+	"data\\SOUND\\SE\\extend.wav",
+	"data\\SOUND\\SE\\vibration.wav",
+	"data\\SOUND\\BGM\\STAGE1.wav",
+	"data\\SOUND\\BGM\\STAGE2.wav",
 };
