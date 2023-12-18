c8d297d166173c629dd2a8b0adc50bfd84b2d74e
RyukiFuziwara <221069@st.yoshida-g.ac.jp>
2023-12-18 09:17:44 +0900

    モードタイトル変更

--- code/Project/Mode/mode_title.cpp
+++ code/Project/Mode/mode_title.cpp
@@ -63,9 +63,10 @@ CMode_Title::CMode_Title(void) {
 	m_StgBoardIdx      = RNLib::Model().Load("data\\MODEL\\Stage_Board.x");
 	m_CoinBoardIdx     = RNLib::Model().Load("data\\MODEL\\Coin_Board.x");
 	m_ArrowIdx         = RNLib::Model().Load("data\\MODEL\\Arrow.x");
+	m_EffTex = RNLib::Texture().Load("data\\TEXTURE\\Effect\\eff_Smoke_001.png");
+
 	m_AnimCnt = NULL;
 	m_RotCnt = 0;
-	m_bStageChange = false;
 	m_bRocketMove = false;
 	m_bRocketRot = false;
 }
@@ -660,6 +661,13 @@ void CMode_Title::StageDraw(int nPlanet, int nStage, D3DXVECTOR3 poscor, float &
 
 			{//ロケット描画
 				RNLib::Model().Put(PRIORITY_OBJECT, m_RocketIdx, m_RocketPosOld + (m_RocketposDiff * RktAnimRt), m_RocketRotOld + (RotRate * m_RocketRotDiff), Scale3D(0.15f, 0.15f, 0.15f), false);
+
+				float ScaleTex = (float)(rand() % (int)(INIT_EFFECT_SCALE.x * 0.2) + 1.0f);
+				D3DXVECTOR3 m_TexPos = D3DXVECTOR3(m_RocketPosOld.x + (m_RocketposDiff.x * RktAnimRt) + (float)(rand() % (int)8 - 8 * 0.5),
+					m_RocketPosOld.y + (m_RocketposDiff.y * RktAnimRt),
+					m_RocketPosOld.z + (m_RocketposDiff.z * RktAnimRt));
+
+				Manager::EffectMgr()->ParticleCreate(m_EffTex, m_TexPos, D3DXVECTOR3(ScaleTex, ScaleTex, 0.0f), Color{ 255,0,255,255 }, CParticle::TYPE::TYPE_FLOATUP,60,INITROT3D);
 			}
 
 			//数字ブロックアニメーション処理
