a7b420d016ee85a3cf0786317e2e6eeac02b88a8
西村吏功 <221052@st.yoshida-g.ac.jp>
2023-12-19 17:44:47 +0900

    関数定義を追加

--- code/_RNLib/RNfunction.h
+++ code/_RNLib/RNfunction.h
@@ -9,6 +9,14 @@
 //****************************************
 // 関数宣言
 //****************************************
+// float値等価比較
+bool  RN_GetIsEq    /* == */(const float& numA, const float& numB, const float& allowableError = 0.000001f);
+bool  RN_GetIsNoEq  /* != */(const float& numA, const float& numB, const float& allowableError = 0.000001f);
+bool  RN_GetIsGtOrEq/* >= */(const float& numA, const float& numB, const float& allowableError = 0.000001f);
+bool  RN_GetIsGt    /* >  */(const float& numA, const float& numB, const float& allowableError = 0.000001f);
+bool  RN_GetIsLsOrEq/* <= */(const float& numA, const float& numB, const float& allowableError = 0.000001f);
+bool  RN_GetIsLs    /* <  */(const float& numA, const float& numB, const float& allowableError = 0.000001f);
+// リソースの読み込み
 short RN_LoadTexture (const char* loadPath);
 short RN_LoadModel   (const char* loadPath);
 short RN_LoadSetUp3D (const char* loadPath);
